2b86e0d61aedada8e220d3e62c6bd6a957e64e9f
Glitch (tasty-teas) <none>
2020-07-09 15:26:16 +0000

    👋📚 Updated with Glitch

--- custom.js
+++ custom.js
@@ -1,10 +1,34 @@
 function getCookie(name) {
-    var v = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
-    return v ? v[2] : null;
+  var v = document.cookie.match("(^|;) ?" + name + "=([^;]*)(;|$)");
+  return v ? v[2] : null;
 }
 function setCookie(name, value, days) {
-    var d = new Date;
-    d.setTime(d.getTime() + 24*60*60*1000*days);
-    document.cookie = name + "=" + value + ";path=/;expires=" + d.toGMTString();
+  var d = new Date();
+  d.setTime(d.getTime() + 24 * 60 * 60 * 1000 * days);
+  document.cookie = name + "=" + value + ";path=/;expires=" + d.toGMTString();
 }
-function deleteCookie(name) { setCookie(name, '', -1); }
+function deleteCookie(name) {
+  setCookie(name, "", -1);
+}
+
+//Add to cart
+function addToCart(item) {
+  if (getCookie(item) == null) {
+    setCookie(item, 1);
+    updatedCartCount();
+  } else {
+    setCookie(item, parseInt(getCookie(item)) + 1);
+    updatedCartCount();
+  }
+}
+
+//Update cart badge count
+function updatedCartCount() {
+  var cartCount = 0;
+  if (getCookie("berryfusion")){cartCount+=Number(getCookie("berryfusion"))}
+  if (getCookie("lemon")){cartCount+=Number(getCookie("lemon"))}
+  if (getCookie("mint")){cartCount+=Number(getCookie("mint"))}
+  if (getCookie("orange")){cartCount+=Number(getCookie("orange"))}
+  if (getCookie("raspberry")){cartCount+=Number(getCookie("raspberry"))}
+  document.getElementsByClassName('badge').value = cartCount;
+}
